b9a8ef908def3124c11247737adcf1ad5a802c05
Arut <159909262+4rutyun@users.noreply.github.com>
2025-03-19 21:59:26 +0300

    task1.py

--- task1/task1.py
+++ task1/task1.py
@@ -1,32 +1,23 @@
+#импорт модуля sys
 import sys
 
+#создание функции кругового массива
 def circular_array_path(n, m):
-    # Создаем круговой массив от 1 до n
-    circular_array = list(range(1, n + 1))
     path = []
     index = 0
-
-    # Пока не вернемся к первому элементу
     while True:
-        # Добавляем начальный элемент интервала в путь
-        path.append(circular_array[index])
-        # Вычисляем следующий индекс, начиная с последнего элемента текущего интервала
+        path.append(index + 1)
         index = (index + m - 1) % n
-        # Если вернулись к первому элементу, выходим из цикла
         if index == 0:
             break
-
     return path
 
+#проверка программы, вывод результата
 if __name__ == "__main__":
-    # Получаем аргументы командной строки
     if len(sys.argv) != 3:
         print("Usage: python task1.py n m")
         sys.exit(1)
 
-    n = int(sys.argv[1])
-    m = int(sys.argv[2])
+    n, m = map(int, sys.argv[1:3])
+    print("".join(map(str, circular_array_path(n, m))))
 
-    # Получаем путь и выводим его
-    path = circular_array_path(n, m)
-    print("".join(map(str, path)))
