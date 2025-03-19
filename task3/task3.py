b4ee9b5e713484fc641fc795a2c3a85ba907428b
Arut <159909262+4rutyun@users.noreply.github.com>
2025-03-19 22:01:26 +0300

    task3.py

--- task3/task3.py
+++ task3/task3.py
@@ -1,42 +1,35 @@
+#импорт модулей
 import json
 import sys
 
+#открытие и загрузка данных из файла json
 def load_json(file_path):
     with open(file_path, 'r') as file:
         return json.load(file)
 
+#заполнение полей value
 def fill_values(tests, values_dict):
     for test in tests:
-        test_id = test.get('id')
-        if test_id in values_dict:
+        if (test_id := test.get('id')) in values_dict:
             test['value'] = values_dict[test_id]
-        # Проверяем оба возможных ключа для вложенных тестов
-        if 'tests' in test:
-            fill_values(test['tests'], values_dict)
-        elif 'values' in test:
-            fill_values(test['values'], values_dict)
+        for key in ('tests', 'values'):
+            if key in test:
+                fill_values(test[key], values_dict)
 
+#проверка программы, загрузка данных из values и tests, запись данных в report
 if __name__ == "__main__":
     if len(sys.argv) != 4:
         print("Usage: python task3.py values.json tests.json report.json")
         sys.exit(1)
 
-    values_file = sys.argv[1]
-    tests_file = sys.argv[2]
-    report_file = sys.argv[3]
+    values_data = load_json(sys.argv[1])
+    tests_data = load_json(sys.argv[2])
 
-    # Загрузка данных из файлов JSON
-    values_data = load_json(values_file)
-    tests_data = load_json(tests_file)
-
-    # Создание словаря для быстрого поиска значений по id
     values_dict = {item['id']: item['value'] for item in values_data['values']}
 
-    # Заполнение значений в tests
     fill_values(tests_data['tests'], values_dict)
 
-    # Запись значений в report.json
-    with open(report_file, 'w') as file:
+    with open(sys.argv[3], 'w') as file:
         json.dump(tests_data, file, indent=4)
 
 
