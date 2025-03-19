01d71a3e1871de1557ea46d5c58fee5b835e1485
Arut <159909262+4rutyun@users.noreply.github.com>
2025-03-19 22:02:45 +0300

    task4.py

--- task4/task4.py
+++ task4/task4.py
@@ -1,20 +1,22 @@
+#импорт модуля
 import sys
 
+#создание функции для открытия файла, считывания и сортировки в список
 def read_numbers(file_path):
     with open(file_path, 'r') as file:
-        return [int(line.strip()) for line in file]
+        return sorted(int(line.strip()) for line in file)
 
+#функция для нахождения медианы, возвращает сумму абсолютных разниц между каждым числом и медианой
 def min_moves_to_equal(nums):
-    nums.sort()
     median = nums[len(nums) // 2]
     return sum(abs(num - median) for num in nums)
 
+#проверка программы, вывод минимального количество ходов
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Usage: python task4.py numbers.txt")
         sys.exit(1)
 
-    numbers_file = sys.argv[1]
-    nums = read_numbers(numbers_file)
-    moves = min_moves_to_equal(nums)
-    print(moves)
+    nums = read_numbers(sys.argv[1])
+    print(min_moves_to_equal(nums))
+
